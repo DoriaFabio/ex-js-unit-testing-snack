d6ecf7f9d4dd012c0500b255cfe15ea28d8d3374
DoriaFabio <doriafabio2001@gmail.com>
2025-05-15 14:18:47 +0200

    do test 3

--- testVari.js
+++ testVari.js
@@ -1,3 +1,4 @@
+//? Funzione che restituisce le iniziali di un nome completo
 function getInitial(nomeCompleto) {
     const parole = nomeCompleto.split(" ");
     let iniziali = "";
@@ -6,10 +7,18 @@ function getInitial(nomeCompleto) {
     }
     return iniziali;
 }
-
+//? Funzione che restituisce la parola in minuscolo
 function createSlug(parola) {
     const lowerparola = parola.toLowerCase();
     return lowerparola;
 }
+//? Funzione che restituisce la media di un array di numeri
+function average(arr) {
+    let somma = 0;
+    for (let i = 0; i < arr.length; i++) {
+        somma += arr[i];
+    }
+    return media = somma / arr.length;
+}
 
-module.exports = {getInitial, createSlug}
+module.exports = {getInitial, createSlug, average}
--- testVari.test.js
+++ testVari.test.js
@@ -10,5 +10,12 @@ test('La funzione createSlug restituisce una stringa in lowercase', () => {
     const result2 = createSlug("Fabio Doria");
     expect(result2).toBe("fabio doria");
 })
+//! Test 3
+test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+    const result = average([1, 2, 3, 4, 5]);
+    expect(result).toBeCloseTo(3);
+    const result2 = average([1, 2, 3, 4, 4]);
+    expect(result2).toBeCloseTo(2.8);
+})
 
-const { getInitial, createSlug } = require("./testVari");
+const { getInitial, createSlug, average } = require("./testVari");
